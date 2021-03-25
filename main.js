03a14deb1f2e99e4dbc87167ddd811073ffb8ff9
Thannasorn <6206021611125@fitm.kmutnb.ac.th>
2021-03-25 10:31:44 +0700

    update

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 0 // new data property //
+            inventory: 100, // new data property //
+            onSale: true
         }
     }
 })
